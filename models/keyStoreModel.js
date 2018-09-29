b808acc2c2deb65eb1e3a9021fb8b32a50b284cf
Matthew Howard <matthew@hattmo.com>
2018-09-29 00:52:24 -0500

    Sign and unsigned option

--- models/keyStoreModel.js
+++ models/keyStoreModel.js
@@ -4,16 +4,21 @@ const fs = require('fs');
 const uuid = require('uuid/v4');
 
 const keygen = {
-
+  /**
+   * Creates the directories temp and keystores if they dont exist
+   */
   checkDirs: () => new Promise((resolve, reject) => {
-    const dirsToCreate = [keygen.createDir('./temp'), keygen.createDir('./cas'), keygen.createDir('./keystores')];
+    const dirsToCreate = [keygen.createDir('./temp'), keygen.createDir('./keystores')];
     Promise.all(dirsToCreate).then(() => {
       resolve();
     }).catch((err) => {
       reject(err);
     });
   }),
-
+  /**
+   * Promisified make directory will resolve if directory exists.
+   * @param {String} dir
+   */
   createDir: dir => new Promise((resolve, reject) => {
     fs.exists(dir, (exists) => {
       if (!exists) {
@@ -28,39 +33,59 @@ const keygen = {
     });
   }),
 
-  generateKeyStore: opt => new Promise((resolve, reject) => {
+  /**
+   * @param {Object} opt
+   * @param {String} opt.alias
+   * @param {String} opt.password
+   * @param {String} opt.keystore
+   * @param {String} uniquepath
+   * @param {Object} ca
+   * @param {String} ca.alias
+   * @param {String} ca.password
+   * @param {String} ca.keystore
+   */
+
+  signKeyStore: (opt, uniquepath, ca) => new Promise((resolve, reject) => {
+    keygen.certreq(opt, uniquepath)
+      .then(() => keygen.gencert(ca, uniquepath))
+      .then(() => keygen.exportcert(ca, uniquepath, 'CA.crt'))
+      .then(() => keygen.importcert({
+        alias: 'CA',
+        password: opt.password,
+        keystore: opt.keystore,
+      }, uniquepath, 'CA.crt'))
+      .then(() => keygen.importcert(opt, uniquepath, 'temp.crt'))
+      .then(() => {
+        resolve();
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  }),
+  /**
+   * @param {Object} opt
+   * @param {String} opt.cn
+   * @param {String} opt.ou
+   * @param {String} opt.o
+   * @param {String} opt.alias
+   * @param {String} opt.password
+   * @param {String} opt.keystore
+   * @param {Object} [ca]
+   * @param {String} [ca.alias]
+   * @param {String} [ca.password]
+   * @param {String} [ca.keystore]
+   */
+  generateKeyStore: (opt, ca) => new Promise((resolve, reject) => {
     const uniquepath = uuid();
-    fsp.mkdir(`temp/${uniquepath}`).then(() => keygen.genkeypair({
-      cn: opt.cn,
-      ou: opt.ou,
-      o: opt.o,
-      password: opt.password,
-      uniquepath,
-    })).then(() => keygen.certreq({
-      keystore: `${opt.cn}.jks`,
-      password: opt.password,
-      uniquepath,
-    })).then(() => keygen.gencert({
-      uniquepath,
-    }))
-      .then(() => keygen.exportcert({
-        uniquepath,
-      }))
-      .then(() => keygen.importcert({
-        keystore: `${opt.cn}.jks`,
-        password: opt.password,
-        file: 'CA.crt',
-        alias: 'cacert',
-        uniquepath,
-      }))
-      .then(() => keygen.importcert({
-        keystore: `${opt.cn}.jks`,
-        password: opt.password,
-        file: 'temp.crt',
-        alias: 'mykey',
-        uniquepath,
-      }))
-      .then(() => fsp.copyFile(`temp/${uniquepath}/${opt.cn}.jks`, `keystores/${opt.cn}.jks`))
+    fsp.mkdir(`temp/${uniquepath}`)
+      .then(() => keygen.genkeypair(opt, uniquepath))
+      .then(() => {
+        if (ca) {
+          return keygen.signKeyStore(opt, uniquepath, ca);
+        }
+        return false;
+      })
+      .then(() => fsp.copyFile(`temp/${uniquepath}/${opt.keystore}.jks`, `keystores/${opt.keystore}.jks`))
       .then(() => {
         resolve();
       })
@@ -80,18 +105,27 @@ const keygen = {
       });
   }),
 
-
-  genkeypair: opt => new Promise((resolve, reject) => {
-    console.log(opt);
+  /**
+   * Creates a keystore at the unique path described in opt.
+   * @param {Object} opt
+   * @param {String} opt.cn
+   * @param {String} opt.ou
+   * @param {String} opt.o
+   * @param {String} opt.alias
+   * @param {String} opt.password
+   * @param {String} opt.keystore
+   * @param {String} uniquepath
+   */
+  genkeypair: (opt, uniquepath) => new Promise((resolve, reject) => {
     exec(`keytool -genkeypair \
-        -alias mykey \
+        -alias ${opt.alias} \
         -keyalg RSA \
         -keysize 2048 \
         -dname "cn=${opt.cn}, ou=${opt.ou}, o=${opt.o}" \
         -validity 365 \
         -keypass ${opt.password} \
         -storepass ${opt.password} \
-        -keystore temp/${opt.uniquepath}/${opt.cn}.jks`, (error) => {
+        -keystore temp/${uniquepath}/${opt.keystore}.jks`, (error) => {
       if (error) {
         reject(error);
       } else {
@@ -99,13 +133,22 @@ const keygen = {
       }
     });
   }),
-  certreq: opt => new Promise((resolve, reject) => {
+  /**
+   * Generates a file called temp.csr at the unique path from the
+   * keystore described in opt. The keystore must also be in the unique path.
+   * @param {Object} opt
+   * @param {String} opt.alias
+   * @param {String} opt.password
+   * @param {String} opt.keystore
+   * @param {String} uniquepath
+   */
+  certreq: (opt, uniquepath) => new Promise((resolve, reject) => {
     exec(`keytool -certreq \
-        -alias mykey \
-        -file temp/${opt.uniquepath}/temp.csr \
+        -alias ${opt.alias} \
+        -file temp/${uniquepath}/temp.csr \
         -keypass ${opt.password} \
         -storepass  ${opt.password}\
-        -keystore temp/${opt.uniquepath}/${opt.keystore}`, (error) => {
+        -keystore temp/${uniquepath}/${opt.keystore}.jks`, (error) => {
       if (error) {
         reject(error);
       } else {
@@ -113,14 +156,25 @@ const keygen = {
       }
     });
   }),
-  gencert: opt => new Promise((resolve, reject) => {
+
+  /**
+   * Signs temp.csr in the unique path with another certificate from keystores.
+   * Outputs a cert named temp.crt in the unique path.
+   *
+   * @param {Object} opt
+   * @param {String} opt.alias - alias of the signing key
+   * @param {String} opt.password - signing keystore password
+   * @param {String} opt.keystore - signing keystore
+   * @param {String} uniquepath
+   */
+  gencert: (opt, uniquepath) => new Promise((resolve, reject) => {
     exec(`keytool -gencert\
-        -alias mykey\
-        -infile temp/${opt.uniquepath}/temp.csr\
-        -outfile temp/${opt.uniquepath}/temp.crt\
-        -keypass password\
-        -storepass password\
-        -keystore cas/CA.jks\
+        -alias ${opt.alias}\
+        -infile temp/${uniquepath}/temp.csr\
+        -outfile temp/${uniquepath}/temp.crt\
+        -keypass ${opt.password}\
+        -storepass ${opt.password}\
+        -keystore keystores/${opt.keystore}.jks\
         -rfc`, (error) => {
       if (error) {
         reject(error);
@@ -129,12 +183,21 @@ const keygen = {
       }
     });
   }),
-  exportcert: opt => new Promise((resolve, reject) => {
+  /**
+   * Exports a cert at alias from keystore to a file named opt.file in the unique path.
+   * @param {Object} opt
+   * @param {String} opt.alias
+   * @param {String} opt.password
+   * @param {String} opt.keystore
+   * @param {String} uniquepath
+   * @param {String} file
+   */
+  exportcert: (opt, uniquepath, file) => new Promise((resolve, reject) => {
     exec(`keytool -exportcert\
-        -alias mykey\
-        -file temp/${opt.uniquepath}/CA.crt\
-        -storepass password\
-        -keystore cas/CA.jks\
+        -alias ${opt.alias}\
+        -file temp/${uniquepath}/${file}\
+        -storepass ${opt.password}\
+        -keystore keystores/${opt.keystore}.jks\
         -rfc`, (error) => {
       if (error) {
         reject(error);
@@ -143,14 +206,25 @@ const keygen = {
       }
     });
   }),
-  importcert: opt => new Promise((resolve, reject) => {
+  /**
+   * Imports a certificate with the file name defined in "opt.file" into
+   * the keystore 'opt.keystore'.
+   * file and keystore must be in the unique path.
+   * @param {Object} opt
+   * @param {String} opt.alias
+   * @param {String} opt.password
+   * @param {String} opt.keystore
+   * @param {String} uniquepath
+   * @param {String} file
+   */
+  importcert: (opt, uniquepath, file) => new Promise((resolve, reject) => {
     exec(`keytool -importcert\
         -noprompt -trustcacerts\
         -alias ${opt.alias}\
-        -file temp/${opt.uniquepath}/${opt.file}\
+        -file temp/${uniquepath}/${file}\
         -keypass ${opt.password}\
         -storepass ${opt.password}\
-        -keystore temp/${opt.uniquepath}/${opt.keystore}`, (error) => {
+        -keystore temp/${uniquepath}/${opt.keystore}.jks`, (error) => {
       if (error) {
         reject(error);
       } else {
--- test/keygentest.js
+++ test/keygentest.js
@@ -3,125 +3,127 @@ const fsp = require('fs').promises;
 const fs = require('fs');
 const keygen = require('../models/keyStoreModel');
 
-describe('keygentest', () => {
-  const testUri = 'test.com';
-  const testPassword = 'password';
+describe('Keygenmodel Test', () => {
+
   const uniquepath = 'mytestpath';
 
-  before((done) => {
-    keygen.checkDirs().then(() => fsp.mkdir(`temp/${uniquepath}`)).then(() => {
-      fs.exists('cas/CA.jks', (exists) => {
-        if (!exists) {
-          fsp.mkdir('temp/tempCA').then(() => keygen.genkeypair({
-            cn: 'CA',
-            ou: 'hattmo',
-            o: 'universe',
-            password: 'password',
-            uniquepath: 'tempCA',
-          })).then(() => fsp.copyFile('temp/tempCA/CA.jks', 'cas/CA.jks')).then(() => fsp.unlink('temp/tempCA/CA.jks'))
-            .then(() => fsp.rmdir('temp/tempCA'))
-            .then(() => {
-              done();
-            })
-            .catch((err) => {
-              done(err);
+  const options = {
+    cn: 'test.com',
+    ou: 'hattmo',
+    o: 'universe',
+    alias: 'mykey',
+    password: 'password',
+    keystore: 'testkeystore',
+  };
+  const caOptions = {
+    cn: 'ca.com',
+    ou: 'hattmo',
+    o: 'universe',
+    alias: 'ca',
+    password: 'password',
+    keystore: 'testcakeystore',
+  }
+
+  before(() => {
+    return keygen.checkDirs()
+      .then(() => fsp.mkdir(`temp/${uniquepath}`))
+  });
+  describe('Partial tests', () => {
+    describe('CA test', () => {
+      describe('genkeypair_CA', () => {
+        it('Should generate a keypair', () => {
+          return keygen.genkeypair(caOptions, uniquepath).then(() => {
+            fs.exists(`./temp/${uniquepath}/${caOptions.keystore}.jks`, (exists) => {
+              assert.ok(exists);
             });
-        } else {
-          done();
-        }
+          });
+        }).timeout(0);
       });
-    }).catch((err) => {
-      done(err);
-    });
-  });
 
-  describe('genkeypair', () => {
-    it('should generate a keypair', () => {
-      const options = {
-        cn: testUri,
-        ou: 'hattmo',
-        o: 'universe',
-        password: testPassword,
-        uniquepath,
-      };
-      return keygen.genkeypair(options).then(() => {
-        fs.exists(`./temp/${uniquepath}/${testUri}.jks`, (exists) => {
-          assert.ok(exists);
+      after(() => fsp.copyFile(`temp/${uniquepath}/${caOptions.keystore}.jks`, `keystores/${caOptions.keystore}.jks`))
+    })
+
+    describe('genkeypair', () => {
+      it('Should generate a keypair', () => {
+        return keygen.genkeypair(options, uniquepath).then(() => {
+          fs.exists(`./temp/${uniquepath}/${options.keystore}.jks`, (exists) => {
+            assert.ok(exists);
+          });
         });
-      });
-    }).timeout(0);
-  });
-  describe('certreq', () => {
-    it('should generate a certreq', () => {
-      const options = {
-        keystore: `${testUri}.jks`,
-        password: testPassword,
-        uniquepath,
-      };
-      return keygen.certreq(options).then(() => {
-        fs.exists(`./temp/${uniquepath}/temp.csr`, (exists) => {
-          assert.ok(exists);
+      }).timeout(0);
+    })
+
+    describe('certreq', () => {
+      it('Should generate a certreq', () => {
+        return keygen.certreq(options, uniquepath).then(() => {
+          fs.exists(`./temp/${uniquepath}/temp.csr`, (exists) => {
+            assert.ok(exists);
+          });
         });
       });
     });
-  });
-  describe('gencert', () => {
-    it('should generate a signed cert', () => keygen.gencert({
-      uniquepath,
-    }).then(() => {
-      fs.exists(`./temp/${uniquepath}/temp.crt`, (exists) => {
-        assert.ok(exists);
-      });
-    }));
-  });
-  describe('exportcert', () => {
-    it('should export the CA cert', () => keygen.exportcert({
-      uniquepath,
-    }).then(() => {
-      fs.exists(`./temp/${uniquepath}/CA.crt`, (exists) => {
-        assert.ok(exists);
-      });
-    }));
-  });
-  describe('importcert', () => {
-    it('should import a CA cert', () => {
-      const options = {
-        keystore: `${testUri}.jks`,
-        password: testPassword,
-        file: 'CA.crt',
-        alias: 'cacert',
-        uniquepath,
-      };
-      return keygen.importcert(options);
-    });
-    it('should import a signed cert', () => {
-      const options = {
-        keystore: `${testUri}.jks`,
-        password: testPassword,
-        file: 'temp.crt',
-        alias: 'mykey',
-        uniquepath,
-      };
-      return keygen.importcert(options);
-    });
-  });
-  describe('generateKeyStore', () => {
-    it('should generate a keystore with a signed cert', () => {
-      const options = {
-        cn: 'fulltest.com',
-        ou: 'hattmo',
-        o: 'universe',
-        password: 'blahblah',
-      };
-      return keygen.generateKeyStore(options);
-    }).timeout(0);
-  });
 
-  after(() => fsp.readdir(`temp/${uniquepath}`).then((files) => {
-    filePromises = [];
-    files.forEach((file) => {
-      filePromises.push(fsp.unlink(`temp/${uniquepath}/${file}`));
+    describe('gencert', () => {
+      it('Should generate a signed cert', () => keygen.gencert(caOptions, uniquepath).then(() => {
+        fs.exists(`./temp/${uniquepath}/temp.crt`, (exists) => {
+          assert.ok(exists);
+        });
+      }));
     });
-    return Promise.all(filePromises);
-  }).then(() => fsp.rmdir(`temp/${uniquepath}`)).then(() => fsp.unlink('keystores/fulltest.com.jks')));
-});
+
+    describe('exportcert', () => {
+      it('Should export the CA cert', () => keygen.exportcert(caOptions, uniquepath, 'CA.crt').then(() => {
+        fs.exists(`./temp/${uniquepath}/CA.crt`, (exists) => {
+          assert.ok(exists);
+        });
+      }));
+    });
+
+    describe('importcert', () => {
+      it('Should import a CA cert', () => keygen.importcert({
+        alias: 'CA',
+        password: options.password,
+        keystore: options.keystore,
+      }, uniquepath, 'CA.crt'));
+      it('should import a signed cert', () => keygen.importcert(options, uniquepath, 'temp.crt'));
+    });
+
+    after(() => fsp.readdir(`temp/${uniquepath}`)
+      .then((files) => {
+        filePromises = [];
+        files.forEach((file) => {
+          filePromises.push(fsp.unlink(`temp/${uniquepath}/${file}`));
+        });
+        return Promise.all(filePromises);
+      })
+      .then(() => fsp.rmdir(`temp/${uniquepath}`)));
+  })
+
+
+  describe('full tests', () => {
+    const fulloptionsUn = {
+      cn: 'unsignedfulltest.com',
+      ou: 'hattmo',
+      o: 'universe',
+      alias: 'mykey',
+      password: 'password',
+      keystore: 'unsignedfulltestkeystore',
+    };
+    const fulloptions = {
+      cn: 'signedfulltest.com',
+      ou: 'hattmo',
+      o: 'universe',
+      alias: 'mykey',
+      password: 'password',
+      keystore: 'signedfulltestkeystore',
+    };
+    describe('generateKeyStore', () => {
+      it('Should generate a keystore with a unsiqned cert', () => keygen.generateKeyStore(fulloptionsUn)).timeout(0);
+      it('Should generate a keystore with a signed cert', () => keygen.generateKeyStore(fulloptions, caOptions)).timeout(0);
+    });
+
+    after(() => fsp.unlink(`keystores/${fulloptionsUn.keystore}.jks`)
+      .then(() => fsp.unlink(`keystores/${fulloptions.keystore}.jks`))
+      .then(() => fsp.unlink(`keystores/${caOptions.keystore}.jks`)));
+  })
+})
