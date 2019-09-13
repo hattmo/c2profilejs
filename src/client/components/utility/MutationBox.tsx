b07e6fd00cb380fbc6a53ce7a95f43e926aee6dd
Matthew Howard <matthew@hattmo.com>
2019-09-12 21:11:47 -0500

    fixed bug in mutation box

--- src/client/components/utility/MutationBox.tsx
+++ src/client/components/utility/MutationBox.tsx
@@ -9,23 +9,31 @@ interface IProps {
     onTerminationChanged: () => void;
 }
 
-export default ({ transform, termination }: IProps, onTransformChanged, onTerminationChanged) => {
+export default ({ transform, termination, onTransformChanged, onTerminationChanged }: IProps) => {
+    console.log(transform);
     return (
         <div>
             {
-                [...transform.map((item, index) => {
-                    <div>
-                        {`${index + 1} ${item.key} ${item.value} `} <span onClick={() => {
-                            onTransformChanged(transform.filter((_item, i) => { index !== i }));
+                transform.map((item, index) => {
+                    return (<div key={index}>
+                        {`${index + 1} ${item.key} ${item.value} `}
+                        <span onClick={() => {
+                            onTransformChanged(transform.filter((_item, i) => index !== i));
                         }}>X</span>
-                    </div>
-                }),
-                termination ? <div>
+                    </div>)
+                })
+            }{
+                termination ? <div key={0}>
                     {`Term: ${termination.key} ${termination.value} `} <span onClick={() => {
                         onTerminationChanged();
                     }}>X</span>
-                </div> : null]
+                </div> : null
             }
         </div >
     )
 }
+
+/**
+ * ,
+
+ */
